492347f1af4fcdfecb45167a1b702d8954ec0bfe
Second-Son-of-Mozzarella <Mohtzee@protonmail.com>
2024-01-31 15:22:32 -0600

    [1.02] Multi-Country Multi-Medal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,87 @@
 import React, { Component } from 'react';
-import './App.css';
 import Country from './Components/Country';
+import Box from '@mui/material/Box';
+import './App.css';
+
 
 
 class App extends Component {
+  state = {
+    countries: [
+      { id: 1, name: 'United States', golds: 12, silvers: 3, bronzes: 1},
+      { id: 2, name: 'China', golds: 2, silvers: 6, bronzes: 6},
+      { id: 3, name: 'Russia', golds: 8, silvers: 4, bronzes: 2},
+    ]
+  }
+
+AddGolds = (locaterId) => {
+    
+    this.setState({ golds: this.state.countries.find(country => country.id === locaterId).golds += 1 })
+}
+
+RemoveGolds = (locaterId) => {
+
+    if (this.state.countries.find(country => country.id === locaterId).golds >= 1) {
+        this.setState({ golds: this.state.countries.find(country => country.id === locaterId).golds -= 1 })
+    } else {
+        alert("you cant have negitive medals")
+    }
+
+}
+
+AddSilvers = (locaterId) => {
+    this.setState({ silvers: this.state.countries.find(country => country.id === locaterId).silvers += 1 })
+}
+
+RemoveSilvers = (locaterId) => {
+    if (this.state.countries.find(country => country.id === locaterId).silvers >= 1) {
+        this.setState({ silvers: this.state.countries.find(country => country.id === locaterId).silvers -= 1 })
+    } else {
+        alert("you cant have negitive medals")
+    }
+
+}
+
+AddBronzes = (locaterId) => {
+    this.setState({ bronzes: this.state.countries.find(country => country.id === locaterId).bronzes += 1 })
+}
+
+RemoveBronzes = (locaterId) => {
+    if (this.state.countries.find(country => country.id === locaterId).bronzes >= 1) {
+        this.setState({ bronzes: this.state.countries.find(country => country.id === locaterId).bronzes -= 1 })
+    } else {
+        alert("you cant have negitive medals")
+    }
+
+}
   render() { 
     return ( 
       <div>
-        <Country />
+
+<Box
+        sx={{
+          display: "flex", justifyContent: "flex-center",
+          gridTemplateColumns: 'repeat(2, 1fr)',
+        }}
+      >
+
+        { this.state.countries.map(indcountry => 
+          
+          <Country
+            key={ indcountry.id } 
+            country={ indcountry } 
+            GAdd={this.AddGolds}
+            SAdd={this.AddSilvers}
+            BAdd={this.AddBronzes}
+            GRemove={this.RemoveGolds}
+            SRemove={this.RemoveSilvers}
+            BRemove={this.RemoveBronzes}
+             />
+        )}
+      </Box>
+        
+        
+        
       </div>
      );
   }
--- src/Components/Country.jsx
+++ src/Components/Country.jsx
@@ -8,14 +8,14 @@ import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 import Avatar from '@mui/material/Avatar';
 import Grid from '@mui/material/Grid';
 import Box from "@mui/material/Box";
-import { amber, yellow, grey, white } from '@mui/material/colors';
+import { amber, yellow, grey } from '@mui/material/colors';
 
 class Country extends Component {
     state = {
-        name: 'United States of America',
-        golds: 0,
-        silvers: 0,
-        bronzes: 0,
+        name: this.props.country.name,
+        golds: this.props.country.golds,
+        silvers: this.props.country.silvers,
+        bronzes: this.props.country.bronzes,
     }
 
     renderMedals(medals) {
@@ -25,67 +25,9 @@ class Country extends Component {
 
     // helper method
 
-    AddGolds = () => {
-        // convert the function to an arrow function
-        // arrow functions inherit 'this' keyword
-        console.log(this.state.golds);
-        // the setState method is inherited from the base Component class
-        // when a component's state is altered, it is re-rendered asynchronously by react
-        this.setState({ golds: this.state.golds += 1 })
-    }
 
-    RemoveGolds = () => {
-        // convert the function to an arrow function
-        // arrow functions inherit 'this' keyword
-        console.log(this.state.golds);
-        // the setState method is inherited from the base Component class
-        // when a component's state is altered, it is re-rendered asynchronously by react
-        if (this.state.golds >= 1) {
-            this.setState({ golds: this.state.golds -= 1 })
-        } else {
-            alert("you cant have negitive medals")
-        }
-
-    }
-
-    AddSilvers = () => {
-        console.log(this.state.silvers);
-        this.setState({ silvers: this.state.silvers += 1 })
-    }
-
-    RemoveSilvers = () => {
-        console.log(this.state.silvers);
-        if (this.state.silvers >= 1) {
-            this.setState({ silvers: this.state.silvers -= 1 })
-        } else {
-            alert("you cant have negitive medals")
-        }
-
-    }
-
-    AddBronzes = () => {
-        // convert the function to an arrow function
-        // arrow functions inherit 'this' keyword
-        console.log(this.state.bronzes);
-        // the setState method is inherited from the base Component class
-        // when a component's state is altered, it is re-rendered asynchronously by react
-        this.setState({ bronzes: this.state.bronzes += 1 })
-    }
-
-    RemoveBronzes = () => {
-        // convert the function to an arrow function
-        // arrow functions inherit 'this' keyword
-        console.log(this.state.bronzes);
-        // the setState method is inherited from the base Component class
-        // when a component's state is altered, it is re-rendered asynchronously by react
-        if (this.state.bronzes >= 1) {
-            this.setState({ bronzes: this.state.bronzes -= 1 })
-        } else {
-            alert("you cant have negitive medals")
-        }
-
-    }
     render() {
+        const { country, GAdd, SAdd, BAdd, GRemove, SRemove, BRemove } = this.props;
         return (
             <Box
                 display="flex"
@@ -96,10 +38,10 @@ class Country extends Component {
             >
                 <div>
                     <Container maxWidth="fit" align="center">
-                        <Card sx={{ width: 400, display: "flex", justifyContent: "flex-center", margin: 10, bgcolor: grey[50] }}  >
+                        <Card sx={{ width: 400, display: "flex", justifyContent: "flex-center", margin: 2, bgcolor: grey[50] }}  >
                             <CardContent>
                                 <Typography variant="h4" component="div">
-                                    {this.state.name}
+                                    {country.name}
                                 </Typography>
                                 <hr></hr>
                                 <Typography variant="body2">
@@ -107,30 +49,30 @@ class Country extends Component {
                                         <Grid item xs={4} align="center">
                                             <Grid container spacing={0}>
                                                 <Grid item xs={8} align="center">
-                                                    <Avatar sx={{ bgcolor: yellow[500], width: 80, height: 80, fontSize: 60 }}>{this.renderMedals(this.state.golds)}</Avatar>
+                                                    <Avatar sx={{ bgcolor: yellow[500], width: 80, height: 80, fontSize: 60 }}>{this.renderMedals(country.golds)}</Avatar>
                                                 </Grid>
                                                 <Grid item xs={8} align="center">
-                                                    <RemoveCircleOutlineIcon onClick={this.RemoveGolds} /> <AddCircleOutline onClick={this.AddGolds} />
-                                                </Grid>
-                                            </Grid>
-                                        </Grid>
-                                        <Grid item xs={4} align="center">
-                                            <Grid container spacing={0}>
-                                                <Grid item xs={8} align="center">
-                                                    <Avatar sx={{ bgcolor: grey[400], width: 80, height: 80, fontSize: 60 }}>{this.renderMedals(this.state.silvers)}</Avatar>
-                                                </Grid>
-                                                <Grid item xs={8} align="center">
-                                                    <RemoveCircleOutlineIcon onClick={this.RemoveSilvers} /> <AddCircleOutline onClick={this.AddSilvers} />
+                                                    <RemoveCircleOutlineIcon onClick={() => GRemove(country.id)} /> <AddCircleOutline onClick={() => GAdd(country.id)} />
                                                 </Grid>
                                             </Grid>
                                         </Grid>
                                         <Grid item xs={4} align="center">
                                             <Grid container spacing={0}>
                                                 <Grid item xs={8} align="center">
-                                                    <Avatar sx={{ bgcolor: amber['A700'], width: 80, height: 80, fontSize: 60 }}>{this.renderMedals(this.state.bronzes)}</Avatar>
+                                                    <Avatar sx={{ bgcolor: grey[400], width: 80, height: 80, fontSize: 60 }}>{this.renderMedals(country.silvers)}</Avatar>
                                                 </Grid>
                                                 <Grid item xs={8} align="center">
-                                                    <RemoveCircleOutlineIcon onClick={this.RemoveBronzes} /> <AddCircleOutline onClick={this.AddBronzes} />
+                                                    <RemoveCircleOutlineIcon onClick={() => SRemove(country.id)} /> <AddCircleOutline onClick={() => SAdd(country.id)} />
+                                                </Grid>
+                                            </Grid>
+                                        </Grid>
+                                        <Grid item xs={4} align="center">
+                                            <Grid container spacing={0}>
+                                                <Grid item xs={8} align="center">
+                                                    <Avatar sx={{ bgcolor: amber['A700'], width: 80, height: 80, fontSize: 60 }}>{this.renderMedals(country.bronzes)}</Avatar>
+                                                </Grid>
+                                                <Grid item xs={8} align="center">
+                                                    <RemoveCircleOutlineIcon onClick={() => BRemove(country.id)} /> <AddCircleOutline onClick={() => BAdd(country.id)} />
                                                 </Grid>
                                             </Grid>
                                         </Grid>
